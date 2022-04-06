6b0fc750feb45875c5b9e004d310f2b326903980
barbararui <cristina.barbara@aluno.ifsp.edu.br>
2022-04-06 09:53:36 -0300

    numeros primos

--- lista1/ex3.php
+++ lista1/ex3.php
@@ -1,10 +1,24 @@
 <?php
     
+    $num;
+    $divisor;
+
     for($num=0;$num<=200;$num++)
     {
-        if()
-        {
-            
+        while($num>=$divisor){
+            $resto = $num%$divisor;
+
+            if($resto==0){
+                $cont=0;
+                $cont++;
+            }
+        }
+
+        if($cont>2){
+            echo "Não é primo.";
+        }
+        else{
+            echo "É primo";
         }
     }
 
