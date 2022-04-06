58563f21f409e3bc75a1dcefb967dd7f518d72f4
barbararui <cristina.barbara@aluno.ifsp.edu.br>
2022-04-06 11:39:32 -0300

    data

---
+++ lista3/ex5.php
@@ -0,0 +1,41 @@
+<?php
+    $data = strtotime('06/06/2022'); // padrao americano mes/dia/ano
+    $dia = date('w',$data);
+    echo $dia;
+
+    if($dia==0)
+    {
+        echo "O dia digitado é domingo";
+    }
+    else
+        if($dia==1)
+        {
+            echo "O dia digitado é segunda-feira";
+        }
+        else
+            if($dia==2)
+            {
+                echo "O dia digitado é terça-feira";
+            }
+            else
+                if($dia==3)
+                {
+                    echo "O dia digitado é quarta-feira";
+                }
+                else
+                    if($dia==4)
+                    {
+                        echo "O dia digitado é quinta-feira";
+                    }
+                    else
+                        if($dia==5)
+                        {
+                            echo "O dia digitado é sexta-feira";
+                        }
+                        else
+                            if($dia==6)
+                            {
+                                echo "O dia digitado é sábado";
+                            }
+
+?>
