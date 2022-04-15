0d2650399dcb7e6ed8a7c1c063ab602113bcbd0b
barbararui <101723143+barbararui@users.noreply.github.com>
2022-04-15 12:56:54 -0300

    Add files via upload

---
+++ lista4/ex3.php
@@ -0,0 +1,136 @@
+<?php
+    $estado = $_GET['estado'];
+    
+    if($estado == "SÃO PAULO" || $estado == "São Paulo" || $estado == "sao paulo")
+    {
+        echo "A capital é: São Paulo";
+    }
+    else
+        if($estado == "RIO DE JANEIRO" || $estado == "Rio de Janeiro" || $estado == "rio de janeiro")
+        {
+        echo "A capital é: Rio de Janeiro";
+        }
+        else
+            if($estado == "MINAS GERAIS" || $estado == "Minas Gerais" || $estado == "minas gerais")
+            {
+            echo "A capital é: Belo Horizonte";
+            }
+            else
+                if($estado == "ESPIRITO SANTO" || $estado == "Espirito Santo" || $estado == "espirito santo")
+                {
+                echo "A capital é: Vitória";
+                }
+                else
+                    if($estado == "SANTA CATARINA" || $estado == "Santa Catarina" || $estado == "santa catarina")
+                    {
+                    echo "A capital é: Florianopolis";
+                    }
+                    else
+                        if($estado == "PARANA" || $estado == "Parana" || $estado == "parana")
+                        {
+                        echo "A capital é: Curitiba";
+                        }
+                        else
+                            if($estado == "ACRE" || $estado == "Acre" || $estado == "acre")
+                            {
+                            echo "A capital é: Rio Branco";
+                            }
+                            else
+                                if($estado == "AMAPA" || $estado == "Amapa" || $estado == "amapa")
+                                {
+                                    echo "A capital é: Macapá";
+                                }
+                                else
+                                    if($estado == "RONDONIA" || $estado == "Rondonia" || $estado == "rondonia")
+                                    {
+                                    echo "A capital é: Porto Velho";
+                                    }
+                                    else
+                                        if($estado == "BAHIA" || $estado == "Bahia" || $estado == "bahia")
+                                        {
+                                        echo "A capital é: Salvador";
+                                        }
+                                        else
+                                            if($estado == "CEARA" || $estado == "Ceara" || $estado == "ceara")
+                                            {
+                                            echo "A capital é: Fortaleza";
+                                            }
+                                            else
+                                                if($estado == "ALAGOAS" || $estado == "Alagoas" || $estado == "alagoas")
+                                                {
+                                                echo "A capital é: Maceió";
+                                                }
+                                                else
+                                                    if($estado == "AMAZONAS" || $estado == "Amazonas" || $estado == "amazonas")
+                                                    {
+                                                    echo "A capital é: Manaus";
+                                                    }
+                                                    else
+                                                        if($estado == "PERNAMBUCO" || $estado == "Pernambuco" || $estado == "pernambuco")
+                                                        {
+                                                        echo "A capital é: Recife";
+                                                        }
+                                                        else
+                                                            if($estado == "RIO GRANDE DO SUL" || $estado == "Rio Grande do Sul" || $estado == "rio grande do sul")
+                                                            {
+                                                            echo "A capital é: Porto Alegre";
+                                                            }
+                                                            else
+                                                                if($estado == "RIO GRANDE DO NORTE" || $estado == "Rio Grande do Norte" || $estado == "rio grande do norte")
+                                                                {
+                                                                echo "A capital é: Natal";
+                                                                }
+                                                                else
+                                                                    if($estado == "MATO GROSSO" || $estado == "Mato Grosso" || $estado == "mato grosso")
+                                                                    {
+                                                                    echo "A capital é: Cuiabá";
+                                                                    }
+                                                                    else
+                                                                        if($estado == "MATO GROSSO DO SUL" || $estado == "Mato Grosso do Sul" || $estado == "mato grosso do sul")
+                                                                        {
+                                                                        echo "A capital é: Campo Grande";
+                                                                        }
+                                                                        else
+                                                                            if($estado == "MARANHAO" || $estado == "Maranhao" || $estado == "maranhao")
+                                                                            {
+                                                                            echo "A capital é: São Luís";
+                                                                            }
+                                                                            else
+                                                                                if($estado == "PARA" || $estado == "Para" || $estado == "para")
+                                                                                {
+                                                                                    echo "A capital é: Belém";
+                                                                                }
+                                                                                else
+                                                                                    if($estado == "SERGIPE" || $estado == "Sergipe" || $estado == "sergipe")
+                                                                                        {
+                                                                                            echo "A capital é: Aracaju";
+                                                                                        }
+                                                                                        else
+                                                                                            if($estado == "RORAIMA" || $estado == "Roraima" || $estado == "roraima")
+                                                                                                {
+                                                                                                    echo "A capital é: Boa Vista";
+                                                                                                }
+                                                                                                else
+                                                                                                    if($estado == "PIAUI" || $estado == "Piaui" || $estado == "piaui")
+                                                                                                        {
+                                                                                                            echo "A capital é: Teresina";
+                                                                                                        }
+                                                                                                        else
+                                                                                                            if($estado == "PARAIBA" || $estado == "Paraiba" || $estado == "paraiba")
+                                                                                                                {
+                                                                                                                    echo "A capital é: João Pessoa";
+                                                                                                                }
+                                                                                                                else
+                                                                                                                    if($estado == "TOCANTINS" || $estado == "Tocantins" || $estado == "tocantins")
+                                                                                                                        {
+                                                                                                                            echo "A capital é: Palmas";
+                                                                                                                        }
+                                                                                                                        else
+                                                                                                                            if($estado == "GOIAS" || $estado == "Goias" || $estado == "goias")
+                                                                                                                                {
+                                                                                                                                    echo "A capital é: Goiania";
+                                                                                                                                }
+        
+                        
+
+?>
