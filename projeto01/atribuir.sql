37fb88f8d1e74d96a7a447c65c2d68ad41d925fe
barbararui <cristina.barbara@aluno.ifsp.edu.br>
2022-04-20 11:42:59 -0300

    cadastro

---
+++ projeto01/atribuir.sql
@@ -0,0 +1,5 @@
+use projeto01;
+insert INTO usuario (nome_usuario, email_usuario, telefone_usuario)
+values ("barbara ruiz", "cristina.barbara@aluno.ifsp.com", "75743463");
+
+select * from usuario;
--- projeto01/cadastro_usuario.php
+++ projeto01/cadastro_usuario.php
@@ -11,4 +11,14 @@ echo "Nome do usuário: " .$nome ."<br>";
 echo "O email do usuário: " .$email ."<br>";
 echo "O telefone do usuário: " .$telefone;
 
+$sql = "INSERT INTO usuario (nome_usuario, email_usuario, telefone_usuario)
+        values ('".$nome. "','".$email."', '".$telefone."')";
+
+        $result = mysqli_query($con, $sql);
+        
+        if($result)
+            echo "Dados inseridos com sucesso";
+            else
+                echo "Erro ao inserir dados: ".mysqli_error($con);
+
 ?>
